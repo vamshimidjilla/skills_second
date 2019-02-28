2b993aa485b145a4961153a5f65c51cfde6fcf59
Raghavendar Midjilla <vamshimidjilla@gmail.com>
2019-02-28 18:26:37 +0530

    skills_secondgit

---
+++ src/skills/Jenkins.java
@@ -0,0 +1,12 @@
+package skills;
+
+public class Jenkins {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("jenkins");
+		System.out.println("jenkins ravali");
+	}
+
+}
